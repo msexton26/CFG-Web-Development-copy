b82b8bb6cfd8f6d85231921724f00f4eca51a8ad
M Sexton <Martin.sexton@mca.attrust.org.uk>
2022-03-15 15:01:58 +0000

    hw

--- 05_JS_2/exercise/starter-code/js/app.js
+++ 05_JS_2/exercise/starter-code/js/app.js
@@ -1,16 +1,16 @@
 
-// FUNCTIONS EXERCISE
+FUNCTIONS EXERCISE
 
-// 1. Write a function that reverses a string
-
-
-// 2. Write a function that sorts a string in alphabetical order
+1. Write a function that reverses a string
 
 
-// 3. Write a function that loops over an array or Strings and capitalises each one before returning the array 
+2. Write a function that sorts a string in alphabetical order
 
 
-// 4. Write a function that console logs the data type of the aruments
+3. Write a function that loops over an array or Strings and capitalises each one before returning the array 
 
 
-// 5. Write a function that returns the length of the longest word in a sentence
+4. Write a function that console logs the data type of the aruments
+
+
+5. Write a function that returns the length of the longest word in a sentence
---
+++ 05_JS_2/lesson/Code along - starter code/js/app.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+console.log("Test")
+
+for(var i = 0; i<5; i++ ){
+    console.log("the value of i is" +i)
+}
+
+var fruit = ["strawberry", "kiwi", "raspberry", "apple"]
+
+for (var count=0; count<fruit.length; count++){
+    console.log("fruit is: " , fruit[count])
+}
+
+// Your code goes here
+
+// Exercise: FUNCTIONS
+
+// Exercise: ARGUMENTS
+
+// Passing Variables as arguments when we invoke the function
+
+// Exercise: CONTROL FLOW
+
+// ADDING LOGIC(code available on slides)
+
+// Exercise: FUNCTION LOGIC
+
+
+// SCOPE (code available on slides)
