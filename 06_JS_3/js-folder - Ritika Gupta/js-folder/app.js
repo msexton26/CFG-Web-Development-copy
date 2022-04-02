cef57524adada51f6dbe22a5717641590e934567
M Sexton <Martin.sexton@mca.attrust.org.uk>
2022-04-02 07:38:35 +0100

    update

---
+++ 06_JS_3/js-folder - Ritika Gupta/js-folder/app.js
@@ -0,0 +1,21 @@
+console.log("Hellloo")
+
+var spiderManNoWayHome ={
+     movietitle: "Spider Man No Way Home",
+     genre: "Action",
+     actors: ["Martin", "Grace", "Tom Holland"],
+     director: "Tom Holland",
+     years: 2022,
+     plot: {
+           
+           context: "In the start of the film....",
+           challenge: "Dramatic stuff happening in the middle",
+           end: "something nice happens"
+     },
+     ratings: 10
+
+}
+console.log(spiderManNoWayHome)
+
+// var theTitleIS = spiderManNoWayHome.movietitle
+// var 
