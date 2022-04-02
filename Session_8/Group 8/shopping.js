0f1cfd414fa3014227abab8f23f9dea0e6d49abd
M Sexton <Martin.sexton@mca.attrust.org.uk>
2022-04-02 07:40:02 +0100

    group 8 project

---
+++ Session_8/Group 8/shopping.js
@@ -0,0 +1,16 @@
+function openWomens() {
+    window.open("shop-now-womens.html");
+}
+
+function openMens() {
+          window.open("shop-now-mens.html");
+}
+
+function openChildrens() {
+          window.open("shop-now-childrens.html");
+        }
+
+function openHome() {
+            window.open("index.html");
+}
+     
